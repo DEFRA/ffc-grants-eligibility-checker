cf82b6bfafe34663901cdf7fd7678c8fd647b9ee
David Barker <david.barker@equalexperts.com>
2024-08-27 10:34:15 +0100

    Initial commit

---
+++ src/http/routes/grantType/grantType.spec.js
@@ -0,0 +1,25 @@
+import { addGrantType, getGrantType } from './grantType.js';
+
+jest.mock('uuid', () => {
+    return {
+        v4: jest.fn(() => 'dummy-id-for-testing')
+    };
+});
+
+describe('Grant Type Tests', () => {
+    it('should add and get grant type', () => {
+        const title = 'Test Title';
+        const description = 'Test Description';
+
+        const expectedGrantType = {
+            id: 'dummy-id-for-testing',
+            title,
+            description,
+        };
+
+        addGrantType(title, description);
+        const actualGrantType = getGrantType();
+
+        expect(actualGrantType).toEqual(expectedGrantType);
+    });
+});

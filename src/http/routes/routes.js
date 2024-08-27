a0f3536ec6703d78ef047a6d34f14011364db6c2
David Barker <david.barker@equalexperts.com>
2024-08-27 16:55:57 +0100

    Added base routing configuration and testing setup
    Signed-off-by: David Barker <david.barker@awaze.co.uk>

---
+++ src/http/routes/routes.js
@@ -0,0 +1,7 @@
+import { routes as grantType } from './grantType/grantType.js'
+
+export function getRouteDefinitions() {
+    return [
+        ...grantType
+    ]
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
 import Hapi from '@hapi/hapi';
+import {app as appConfig, views as viewConfig} from "./config";
 import { getGrantType } from './http/routes/grantType/grantType.js';
+import path from "path";
+import {compile, configure} from "nunjucks";
+import {getRouteDefinitions} from "./http/routes/routes.js";
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
+        port: appConfig.port,
+        host: appConfig.host,
     });
 
-    server.route({
-        method: 'GET',
-        path: '/{grantType}',
-        handler: () => {
-            return getGrantType();
+    const routes = getRouteDefinitions();
+    routes.forEach(route => server.route(route))
+
+    server.views({
+        engines: {
+            njk: {
+                compile: (src, options) => {
+                    const template = compile(src, options.environment)
+                    return context => template.render(context)
+                }
+            }
         },
-    });
+        relativeTo: __dirname,
+        compileOptions: {
+            environment: configure(viewConfig.paths)
+        },
+        //@todo need to review this, it will be fine for now but could trip us up later on
+        path: viewConfig.paths[0],
+        context: {
+            version: appConfig.version,
+            assets: viewConfig.assets.app,
+            govAssets: viewConfig.assets.gov,
+            serviceName: appConfig.name,
+            //@todo this can no longer come from config. Must come from the grant type data.
+            pageTitle: appConfig.name,
+            // googleTagManagerKey: config.googleTagManagerKey,
+            // analyticsTagKey: config.analyticsTagKey
+        }
+    })
 
     await server.start();
     console.log('Server running on %s', server.info.uri);

81ffa04f9e0ef319c75feed885cdb09b92cda641
DavidBarker-EE <david.barker@equalexperts.com>
2024-09-16 17:20:09 +0100

    - Added base template for all checkers - Removed webpack - Removed build sequences and replace with sass cli - Added route handler for basic template - Updated dockerfile with correct node version - Added static file serving routes to HAPI
    Signed-off-by: David Barker <david.barker@equalexperts.com>

--- src/index.js
+++ src/index.js
@@ -1,85 +1,67 @@
-import Hapi from '@hapi/hapi'
-import { app as appConfig, views as viewConfig } from './config'
-import { compile, configure } from 'nunjucks'
-import { getRouteDefinitions } from './http/routes/routes.js'
+import Hapi from '@hapi/hapi';
+import {app as appConfig, views as viewConfig} from "./config/index.js";
+import path from "path";
+import njk from "nunjucks";
+import vision from "vision";
+import inert from "@hapi/inert";
+import {getRouteDefinitions} from "./http/routes/routes.js";
 
-/**
- * An async function that initializes the Hapi server
- * @returns {Promise} A promise that resolves when the server is started
- */
 const init = async () => {
-  // Create the server
-  const server = Hapi.server({
-    // Set the server port
-    port: appConfig.port,
-    // Set the server host
-    host: appConfig.host
-  })
+    const server = Hapi.server({
+        port: appConfig.port,
+        host: appConfig.host,
+    });
 
-  // Get the route definitions
-  const routes = getRouteDefinitions()
+    await server.register(inert);
+    await server.register(vision);
 
-  // Add the routes to the server
-  routes.forEach(route => server.route(route))
-
-  // Configure the view engine
-  server.views({
-    engines: {
-      njk: {
-        /**
-         * Compiles a template and returns a function that renders the template with a given context
-         * @param {string} src - The source of the template
-         * @param {object} options - The options for the compilation
-         * @param {object} options.environment - The environment for the compilation
-         * @returns {(context: object) => string} - A function that renders the template with a given context
-         */
-        compile: (src, options) => {
-          // Compile the template
-          const template = compile(src, options.environment)
-
-          // Return a function that renders the template
-          return context => template.render(context)
+    server.route({
+        method: 'GET',
+        path: '/stylesheets/{file*}',
+        handler: {
+            directory: {
+                path: path.resolve(import.meta.dirname, '..', 'public', 'stylesheets')
+            }
         }
-      }
-    },
-    // Set the relative directory
-    relativeTo: __dirname,
-    // Set the compile options
-    compileOptions: {
-      // Set the environment
-      environment: configure(viewConfig.paths)
-    },
-    // Set the path
-    // @todo need to review this, it will be fine for now but could trip us up later on
-    path: viewConfig.paths[0],
-    // Set the context
-    context: {
-      // Set the version
-      version: appConfig.version,
-      // Set the assets
-      assets: viewConfig.assets.app,
-      // Set the gov assets
-      govAssets: viewConfig.assets.gov,
-      // Set the service name
-      serviceName: appConfig.name,
-      // Set the page title
-      // @todo this can no longer come from config. Must come from the grant type data.
-      pageTitle: appConfig.name
-      // googleTagManagerKey: config.googleTagManagerKey,
-      // analyticsTagKey: config.analyticsTagKey
-    }
-  })
+    });
 
-  // Start the server
-  await server.start()
+    const routes = getRouteDefinitions();
+    routes.forEach(route => server.route(route))
 
-  // Log a message to the console
-  console.log('Server running on %s', server.info.uri)
-}
+    server.views({
+        engines: {
+            njk: {
+                compile: (src, options) => {
+                    const template = njk.compile(src, options.environment)
+                    return context => template.render(context)
+                }
+            }
+        },
+        relativeTo: path.resolve(import.meta.dirname, '..'),
+        compileOptions: {
+            environment: njk.configure(viewConfig.paths)
+        },
+        //@todo need to review this, it will be fine for now but could trip us up later on
+        path: viewConfig.paths[0],
+        context: {
+            version: appConfig.version,
+            assets: viewConfig.assets.app,
+            govAssets: viewConfig.assets.gov,
+            serviceName: appConfig.name,
+            //@todo this can no longer come from config. Must come from the grant type data.
+            pageTitle: appConfig.name,
+            // googleTagManagerKey: config.googleTagManagerKey,
+            // analyticsTagKey: config.analyticsTagKey
+        }
+    })
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
 
 process.on('unhandledRejection', (err) => {
-  console.log(err)
-  process.exit(1)
-})
+    console.log(err);
+    process.exit(1);
+});
 
-init().then(r => console.log(r))
+init().then(r => console.log(r));

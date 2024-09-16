14c2e24e5218ba544ae0df3c8bccb710d5a174e3
Andrew Folga <andrew.folga@equalexperts.com>
2024-09-16 12:46:30 +0100

    Add standard

--- src/http/routes/grantType/grantType.js
+++ src/http/routes/grantType/grantType.js
@@ -1,28 +1,28 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from 'uuid'
 
-let grantType;
+let grantType
 
 export const addGrantType = (title, description) => {
-    grantType = {
-        id: uuid(),
-        title,
-        description,
-    };
+  grantType = {
+    id: uuid(),
+    title,
+    description
+  }
 
-    return grantType;
-};
+  return grantType
+}
 
-export const getGrantType = () => grantType;
+export const getGrantType = () => grantType
 
 export const routes = [
-    {
-        method: 'GET',
-        path: '/{grantType}',
-        handler: getGrantType,
-    },
-    {
-        method: 'POST',
-        path: '/{grantType}',
-        handler: addGrantType.bind(this, 'Test Title', 'Test Description'),
-    }
+  {
+    method: 'GET',
+    path: '/{grantType}',
+    handler: getGrantType
+  },
+  {
+    method: 'POST',
+    path: '/{grantType}',
+    handler: addGrantType.bind(this, 'Test Title', 'Test Description')
+  }
 ]

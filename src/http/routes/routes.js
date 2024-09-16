880c6841ebdfde15f646679e23c651aea3473f43
Andrew Folga <andrew.folga@equalexperts.com>
2024-09-16 22:54:02 +0100

    Add linter for jsdoc and fix code

--- src/http/routes/routes.js
+++ src/http/routes/routes.js
@@ -1,5 +1,9 @@
 import { routes as grantType } from './grantType/grantType.js'
 
+/**
+ * A function that returns all the route definitions
+ * @returns {object[]} An array of route definitions
+ */
 export function getRouteDefinitions () {
   return [
     ...grantType

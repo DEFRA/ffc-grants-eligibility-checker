aff6e025616af7438e8f9381c4b49af94b1bd6b5
Andrew Deacon <82759990+ScenarioFactory@users.noreply.github.com>
2024-09-24 11:55:42 +0100

    Make acceptance tests run in Jenkins pipeline (#23)

--- test/acceptance/wdio.conf.js
+++ test/acceptance/wdio.conf.js
@@ -87,7 +87,7 @@ export const config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: process.env.ROOT_URL,
+    baseUrl: process.env.TEST_ENVIRONMENT_ROOT_URL,
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,

e7df1a0e12696ab44a8e70a7ab49690276cfa537
Stephen Powell <swdpcomputing@outlook.com>
2024-10-11 15:23:15 +0100

    import pkg config explicitly to avoid specific experimental warning flag

--- src/config/app.js
+++ src/config/app.js
@@ -1,5 +1,12 @@
-import pkg from "../../package.json" with { type: "json" };
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import path from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const require = createRequire(import.meta.url);
+
+const pkg = require(path.join(__dirname, "../../package.json"));
 export const app = {
   // Application data
   name: pkg.name,

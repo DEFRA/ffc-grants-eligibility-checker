c8efd16d716c7609b2b32683757d3b3473b73142
Stephen Powell <swdpcomputing@outlook.com>
2024-10-18 14:04:47 +0100

    update linting and testing config

--- test/acceptance/steps/given.js
+++ test/acceptance/steps/given.js
@@ -1,18 +1,17 @@
-import { Given } from "@wdio/cucumber-framework";
-import { browser } from "@wdio/globals";
+import { Given } from '@wdio/cucumber-framework';
+import { browser } from '@wdio/globals';
 
-Given(/^a placeholder step/, async () => {
-});
+Given(/^a placeholder step/, async () => {});
 
 Given(/^the user navigates to "([^"]*)?"$/, async (page) => {
-    await browser.url(page);
+  await browser.url(page);
 });
 
 Given(/^completes any login process/, async () => {
-    const currentUrl = await browser.getUrl();
-    if (currentUrl.endsWith("login")) {
-        await $("//input[@id='username']").setValue(process.env.LOGIN_USERNAME);
-        await $("//input[@id='password']").setValue(process.env.LOGIN_PASSWORD);
-        await $("//button[@type='submit']").click();
-    }
+  const currentUrl = await browser.getUrl();
+  if (currentUrl.endsWith('login')) {
+    await $("//input[@id='username']").setValue(process.env.LOGIN_USERNAME);
+    await $("//input[@id='password']").setValue(process.env.LOGIN_PASSWORD);
+    await $("//button[@type='submit']").click();
+  }
 });

1dcad4fd95438618cdf9277fc8406dcb77ecd2e2
Andrew Folga <andrew.folga@equalexperts.com>
2024-10-29 11:24:35 +0000

    Temporarily ignore sonar for error-pages

--- src/config/plugins/error-pages.js
+++ src/config/plugins/error-pages.js
@@ -1,46 +1,57 @@
+// istanbul ignore file
+// TODO: Add narrow integration tests
+
+import statusCodes, {
+  BAD_REQUEST,
+  FORBIDDEN,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND
+} from '../../constants/status-codes';
+
 /*
  * Add an `onPreResponse` listener to return error pages
  */
 export default {
   plugin: {
-    name: "error-pages",
+    name: 'error-pages',
     /**
      * Register the `onPreResponse` listener
      * @param {object} server - The server object
      * @param {object} _options - The options
      */
     register: (server, _options) => {
-      server.ext("onPreResponse", (request, h) => {
+      server.ext('onPreResponse', (request, h) => {
         const response = request.response;
 
         if (response.isBoom) {
-          // console.warn(`response.message=${response.message}`);
-          // console.warn(`response=${JSON.stringify(response, null, 2)}`);
           // An error was raised during
           // processing the request
           const statusCode = response.output.statusCode;
-          if (statusCode === 404) {
-            return h.view("404", response).code(statusCode).takeover();
+          if (statusCode === NOT_FOUND) {
+            return h.view(NOT_FOUND, response).code(statusCodes(NOT_FOUND)).takeover();
           }
           const err = {
             statusCode,
             data: response.data,
-            message: response.message,
+            message: response.message
           };
-          console.error("error", err);
+          console.error('error', err);
 
-          if (statusCode === 400) {
-            return h.view("400", response).code(statusCode).takeover();
+          if (statusCode === BAD_REQUEST) {
+            return h.view(BAD_REQUEST, response).code(statusCodes(BAD_REQUEST)).takeover();
           }
 
-          if (statusCode === 403 || response.message.includes("support ID")) {
-            return h.view("403", response).code(statusCode).takeover();
+          if (statusCode === FORBIDDEN || response.message.includes('support ID')) {
+            return h.view(FORBIDDEN, response).code(statusCodes(FORBIDDEN)).takeover();
           }
           // The return the `500` view
-          return h.view("500", response).code(statusCode).takeover();
+          return h
+            .view(INTERNAL_SERVER_ERROR, response)
+            .code(statusCodes(INTERNAL_SERVER_ERROR))
+            .takeover();
         }
         return h.continue;
       });
-    },
-  },
+    }
+  }
 };
--- src/constants/status-codes.js
+++ src/constants/status-codes.js
@@ -1,5 +1,8 @@
 export const OK = Symbol('OK');
+export const BAD_REQUEST = Symbol('BAD_REQUEST');
+export const FORBIDDEN = Symbol('FORBIDDEN');
 export const NOT_FOUND = Symbol('NOT_FOUND');
+export const INTERNAL_SERVER_ERROR = Symbol('INTERNAL_SERVER_ERROR');
 
 /**
  * A map of status codes and their corresponding HTTP status code values.
@@ -14,10 +17,28 @@ const statusCodeMap = new Map();
 statusCodeMap.set(OK, 200); // NOSONAR:S109 - Allow magic number
 
 /**
+ * The HTTP status code for a bad request.
+ * @constant {number}
+ */
+statusCodeMap.set(BAD_REQUEST, 400); // NOSONAR:S109 - Allow magic number
+
+/**
+ * The HTTP status code for a forbidden request.
+ * @constant {number}
+ */
+statusCodeMap.set(FORBIDDEN, 403); // NOSONAR:S109 - Allow magic number
+
+/**
  * The HTTP status code for a resource that was not found.
  * @constant {number}
  */
 statusCodeMap.set(NOT_FOUND, 404); // NOSONAR:S109 - Allow magic number
+
+/**
+ * The HTTP status code for a server error.
+ * @constant {number}
+ */
+statusCodeMap.set(INTERNAL_SERVER_ERROR, 500); // NOSONAR:S109 - Allow magic number
 
 /**
  * Returns a HTTP status code based on the code given.

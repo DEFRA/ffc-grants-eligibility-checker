67dc2110119a50541d2509d2f163d7e057cfe685
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-10-30 13:37:31 +0000

    Add trace

--- test/acceptance/steps/given.js
+++ test/acceptance/steps/given.js
@@ -2,5 +2,6 @@ import { Given } from '@wdio/cucumber-framework';
 import { browser } from '@wdio/globals';
 
 Given(/^the user navigates to "([^"]*)?"$/, async (page) => {
+  console.log(`process.env.TEST_ENVIRONMENT_ROOT_URL: ${process.env.TEST_ENVIRONMENT_ROOT_URL}`);
   await browser.url(page);
 });

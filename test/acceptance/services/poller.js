bd67691ce37d30411a681f10be68d63535fd3f37
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-11-20 23:24:38 +0000

    Add Input Validation acceptance test

---
+++ test/acceptance/services/poller.js
@@ -0,0 +1,14 @@
+export default class poller {
+  static pollingIntervalSeconds = 1;
+
+  static async pollForSuccess(predicate, pollingLimit = 30) {
+    for (let i = 0; i < pollingLimit; i++) {
+      if (await predicate()) {
+        return true;
+      }
+      await new Promise((resolve) => setTimeout(resolve, this.pollingIntervalSeconds * 1000));
+    }
+
+    return false;
+  }
+}

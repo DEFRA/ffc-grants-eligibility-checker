ba59804adff738f5516e38a9769b75582ec01b21
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-11-28 14:46:01 +0000

    Rename accessibility test file

--- test/accessibility/wdio.conf.js
+++ test/accessibility/wdio.conf.js
@@ -22,7 +22,7 @@ export let config = {
   // The path of the spec files will be resolved relative from the directory of
   // of the config file unless it's absolute.
   //
-  specs: ['./specs/*.spec.js'],
+  specs: ['./specs/*.js'],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'

618d7ed1aa4b6133cbe2f29efc21d60d7de6a11e
Andrew Folga <andrew.folga@equalexperts.com>
2024-11-29 14:10:15 +0000

    Ignore coverage

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -4,8 +4,6 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { schema } from './app-schema';
 
-// Define config schema
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const require = createRequire(import.meta.url);
--- src/config/cache-schema.js
+++ src/config/cache-schema.js
@@ -1,3 +1,4 @@
+// istanbul ignore file
 import Joi from 'joi';
 
 export const schema = Joi.object({

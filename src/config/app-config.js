3813b8ba5847e348134d27e92618e4d06ad21547
Andrew Folga <andrew.folga@equalexperts.com>
2024-11-29 17:54:59 +0000

    Randomise hapi server port in integration tests

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -10,6 +10,14 @@ const require = createRequire(import.meta.url);
 
 const pkg = require(path.join(__dirname, '../../package.json'));
 
+/**
+ * Generates a random port number within the range of 1024 to 65535.
+ * This is useful for dynamically assigning an available port for server instances.
+ * Note that the port range is chosen to avoid privileged ports (below 1024).
+ * @returns {number} A random port number.
+ */
+const getRandomPort = () => Math.floor(Math.random() * (65535 - 1024) + 1024); // Random port between 1024 and 65535
+
 const config = {
   // Application data
   name: pkg.name,
@@ -17,7 +25,7 @@ const config = {
   version: pkg.version,
 
   host: '0.0.0.0',
-  port: 3000,
+  port: process.env.PORT === '0' ? getRandomPort() : process.env.PORT,
   siteTitle: process.env.SITE_TITLE || 'FFC Grants Eligibility Checker',
   surveyLink: process.env.SURVEY_LINK || 'https://example.com/survey',
   sessionTimeoutInMins: process.env.SESSION_TIMEOUT_IN_MINS || '60',

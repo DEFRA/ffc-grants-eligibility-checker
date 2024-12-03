8b4e3f92d912230693737aea27f2cdc0cfa77492
Andrew Folga <andrew.folga@equalexperts.com>
2024-12-03 17:55:08 +0000

    Force the running process to always specify PORT so that npm test always generate a random port

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -25,7 +25,7 @@ const config = {
   version: pkg.version,
 
   host: '0.0.0.0',
-  port: process.env.PORT === '0' ? getRandomPort() : process.env.PORT || '3000',
+  port: process.env.PORT === '0' ? getRandomPort() : process.env.PORT,
   siteTitle: process.env.SITE_TITLE || 'FFC Grants Eligibility Checker',
   surveyLink: process.env.SURVEY_LINK || 'https://example.com/survey',
   sessionTimeoutInMins: process.env.SESSION_TIMEOUT_IN_MINS || '60',

dba9a6528c9b11a86b1ca941ad340164bbf37716
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-12-11 13:05:12 +0000

    Get reporting working again

--- test/performance/run.sh
+++ test/performance/run.sh
@@ -1,3 +1,3 @@
 rm -r -f ./html-reports
-docker-compose -f ../../docker-compose.yaml -f docker-compose.jmeter.yaml run jmeter-test
+docker-compose -f ../../docker-compose.yaml -f docker-compose.jmeter.yaml run --build --rm jmeter-test
 read -t 3 -p "Exiting..."

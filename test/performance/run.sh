0adf0663f9a30af99c03c1ba05a82d2230e01e1a
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-12-15 08:55:30 +0000

    Refactor bash script to call base image entrypoint

--- test/performance/run.sh
+++ test/performance/run.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
-# script is an adaptation of the below entrypoint of the base image, with return code added
-# https://github.com/alpine-docker/jmeter/blob/master/entrypoint.sh
 
-# clear down old output after volumes mounted
+# clear down any local output after volumes mounted
 rm -r -f /html-reports/*
 
-# set up JVM
-set -e
-freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
-s=$(($freeMem/10*8))
-x=$(($freeMem/10*8))
-n=$(($freeMem/10*2))
-export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
-
-# execute jmeter with passed args
-jmeter $@
+# call base image entrypoint with jmeter parameters
+bash /entrypoint.sh -n -t test-plan.jmx -l /html-reports/results.csv -e -o /html-reports -Jjmeter.save.saveservice.assertion_results_failure_message=true
 
 # exit non-zero if failures occurred
 if grep -q ',false,' /html-reports/results.csv; then
-    echo "FAILURES FOUND IN RESULTS"
+    echo "RESULTS CONTAIN FAILURES, EXITING NON-ZERO"
     exit 1
 fi

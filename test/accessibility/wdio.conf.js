3f8878bdb34cf54138afc8ab05bb0339d54e72ed
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-12-16 12:12:33 +0000

    Reduce logging level

--- test/accessibility/wdio.conf.js
+++ test/accessibility/wdio.conf.js
@@ -63,7 +63,7 @@ export let config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'info',
+  logLevel: 'warn',
   //
   // Set specific log levels per logger
   // loggers:

419c639a0c4c92c1830c5446f6e1105386c607c6
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-12-18 08:16:53 +0000

    Refactor

---
+++ test/accessibility/services/functionRetrier.js
@@ -0,0 +1,39 @@
+/**
+ *
+ */
+export default class functionRetrier {
+  /**
+   * Retries a function while an error is being thrown with a specified errorMessage for a given number of times.
+   * @param {Function} fn - The function to be retried.
+   * @param {string} errorMessage - The error message to look for when deciding to retry.
+   * @param {number} retries - The number of retries to make before throwing the error. Optional, defaults to 10.
+   * @returns {object} The result of the function.
+   */
+  static async retryWhileError(fn, errorMessage, retries = 10) {
+    let attempts = 0;
+
+    do {
+      attempts++;
+
+      try {
+        console.warn('functionRetrier: Calling fn on attempt ' + attempts);
+        return await fn();
+      } catch (error) {
+        console.warn('functionRetrier: Caught error ' + error.errorMessage);
+        if (error.errorMessage !== errorMessage) {
+          console.warn('functionRetrier: Throwing unexpected error');
+          throw error;
+        }
+
+        if (attempts === retries) {
+          console.warn('functionRetrier: Throwing expected error after limit reached');
+          throw error;
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        console.warn('functionRetrier: Retrying after waiting');
+        continue;
+      }
+    } while (true);
+  }
+}
--- test/accessibility/specs/example-grant-journey.js
+++ test/accessibility/specs/example-grant-journey.js
@@ -3,42 +3,22 @@ import AxeBuilder from '@axe-core/webdriverio';
 import fs from 'fs-extra';
 import jsonFile from 'jsonfile';
 import path from 'node:path';
-import poller from '../services/poller.js';
+import functionRetrier from '../services/functionRetrier.js';
 
 describe('Example Grant journey', () => {
   it('should meet accessibility standards', async () => {
-    const analyzeAccessibility = async () => {
-      await poller.pollWhileErrorThrownWithMessage(
+    /**
+     * Analyzes the current browser page for accessibility issues.
+     */
+    async function analyzeAccessibility() {
+      const success = await functionRetrier.retryWhileError(
         callAxeAndWriteResults,
         'Page/Frame is not ready'
       );
-    };
+      testFailed = testFailed || !success;
+    }
 
-    const callAxeAndWriteResults = async () => {
-      const results = await new AxeBuilder({ client: browser })
-        .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
-        .analyze();
-
-      const urlParts = (await browser.getUrl()).split('/');
-      let page = urlParts.pop();
-      if (await $(`//div[@class='govuk-error-summary']`).isDisplayed()) {
-        page = `${page}-error`;
-      }
-      const grantName = urlParts.pop();
-
-      fs.ensureDirSync(path.resolve('/home/node/test-output', 'axe-reports', grantName));
-      jsonFile.writeFileSync(
-        path.resolve(`/home/node/test-output/axe-reports/${grantName}`, `${page}.json`),
-        results,
-        { spaces: 4 }
-      );
-
-      if (results.violations.length > 0) {
-        hasViolations = true;
-      }
-    };
-
-    let hasViolations = false;
+    let testFailed = false;
 
     // start
     await browser.url('eligibility-checker/example-grant/start');
@@ -61,6 +41,32 @@ describe('Example Grant journey', () => {
     // confirmation
     await analyzeAccessibility();
 
-    expect(hasViolations).toBe(false);
+    expect(testFailed).toBe(false);
   });
+
+  /**
+   * Calls axe-core, analyzes the current browser page and writes the results to a JSON file.
+   * @returns {boolean} True if Axe analysis was successful, false if accessibility violations were found.
+   */
+  async function callAxeAndWriteResults() {
+    const results = await new AxeBuilder({ client: browser })
+      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+      .analyze();
+
+    const urlParts = (await browser.getUrl()).split('/');
+    let page = urlParts.pop();
+    if (await $(`//div[@class='govuk-error-summary']`).isDisplayed()) {
+      page = `${page}-error`;
+    }
+    const grantName = urlParts.pop();
+
+    fs.ensureDirSync(path.resolve('/home/node/test-output', 'axe-reports', grantName));
+    jsonFile.writeFileSync(
+      path.resolve(`/home/node/test-output/axe-reports/${grantName}`, `${page}.json`),
+      results,
+      { spaces: 4 }
+    );
+
+    return results.violations.length === 0;
+  }
 });

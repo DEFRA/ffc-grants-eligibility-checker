9ef304097a92b2aad87a47303651a5296428b821
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-12-20 00:02:42 +0000

    Refactor out superfluous continue statement

--- test/accessibility/utils/functionRetrier.js
+++ test/accessibility/utils/functionRetrier.js
@@ -16,7 +16,6 @@ export const functionRetrier = async (fn, retries = 10) => {
         throw error;
       }
       await new Promise((resolve) => setTimeout(resolve, 1000));
-      continue;
     }
   } while (true);
 };
